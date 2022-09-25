aefb9df7b8e71cd231c5d9f5ec797f4ab98a97f8
Vinicius Assis <viniciusassis.dev@hotmail.com>
2022-09-25 15:01:43 -0300

    chore: add jest-styled-components

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,20 @@
+import * as S from './styles'
+
 const Main = ({ title = 'React Avancado!' }) => (
-  <main>
-    <h1>{title}</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avancado escrito ao lado."
+    />
+    <S.Title>{title}</S.Title>
+    <S.Description>
+      Typescript, ReactJS, NextJS e Styled Components
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para uma tela com codigo."
+    />
+  </S.Wrapper>
 )
 
 export default Main
